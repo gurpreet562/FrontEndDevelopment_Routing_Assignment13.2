865abddf9d5740d570c75d3ea73bdb7423a2b8d9
gurpreet562 <kgurpreet562@gmail.com>
2017-10-14 21:48:44 +0530

    Create app-routing.module.ts

---
+++ app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+			import {RouterModule,Routes} from '@angular/router';
+			import {AddComponent} from './department-list.component';
+			import{ViewComponent} from './employee-list.component';
+			
+			const routes:Routes=[
+			     {path:'add',component:AddComponent},
+			      {path:'view',component:ViewComponent},
+				      {path:'add/:id',component:AddDetailComponent}
+			]
+			
+			@NgModule({
+			    imports: [
+			        RouterModule.forRoot(routes)
+			    ],
+			    exports:[
+			        RouterModule
+			    ]
+			})
+			export class AppRoutingModule{}
+			export const routingComponents=[AddComponent,ViewComponent,AddDetailComponent]
+
