756e5d823e5269829bcb8655522ab6aa0104f0ea
gurpreet562 <kgurpreet562@gmail.com>
2017-10-14 21:47:36 +0530

    Create add.component.ts

---
+++ add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+			
+			@Component({
+			  selector: 'add-root',
+			  template: `<h1>Adding Data</h1>`
+			               
+			  
+			})
+			export class AddComponent {
+			
+			}
+
